2c08ebc4555fb93f447a6459fcaeae91d922608a
S.B <30941141+s-b-repo@users.noreply.github.com>
2024-11-18 13:33:40 +0200

    Update cli.py

--- fsociety/web_apps/cli.py
+++ fsociety/web_apps/cli.py
@@ -1,11 +1,10 @@
-# Core
 from fsociety.core.menu import tools_cli
 
 from .photon import photon
 from .xsstrike import xsstrike
+from .gobuster import gobuster  # Import gobuster module
 
-__tools__ = [xsstrike, photon]
-
+__tools__ = [xsstrike, photon, gobuster]  # Add gobuster to the tools list
 
 def cli():
     tools_cli(__name__, __tools__)
